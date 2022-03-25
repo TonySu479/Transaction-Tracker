61f2d85eac686b5030e4220de525d5c0bd6905f3
Tony <tonysu479@gmail.com>
2022-03-25 19:32:44 +0100

    transactions page transaction add remove edit

--- TransactionTrackerAngular/src/app/components/transactions/transaction-dialog/transaction-dialog.component.ts
+++ TransactionTrackerAngular/src/app/components/transactions/transaction-dialog/transaction-dialog.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import {TransactionService} from "../../../service/transactionservice";
+import {DynamicDialogConfig, DynamicDialogRef} from "primeng/dynamicdialog";
 
 @Component({
-  selector: 'app-transaction-dialog',
-  templateUrl: './transaction-dialog.component.html',
-  styleUrls: ['./transaction-dialog.component.scss']
+    selector: 'app-transaction-dialog',
+    templateUrl: './transaction-dialog.component.html',
+    styleUrls: ['./transaction-dialog.component.scss']
 })
 export class TransactionDialogComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit(): void {
-  }
+    transactionForm: FormGroup;
+
+    constructor(private ref: DynamicDialogRef,
+                private transactionService: TransactionService,
+                private config: DynamicDialogConfig,
+                private formBuilder: FormBuilder
+    ) {
+    }
+
+    get name() {
+        return this.transactionForm.controls.name;
+    }
+
+    get description() {
+        return this.transactionForm.controls.description;
+    }
+
+    ngOnInit()
+        :
+        void {
+        this.transactionForm = this.formBuilder.group({
+            name: new FormControl(this.config.data.type === "new" ? "" : this.config.data.transaction.name,
+                [Validators.maxLength(20), Validators.required]),
+            description: new FormControl(this.config.data.type === "new" ? "" : this.config.data.transaction.description,
+                [Validators.maxLength(400), Validators.required])
+        });
+    }
+
+    submit() {
+        if (!this.transactionForm.valid) {
+            this.transactionForm.markAllAsTouched();
+            return;
+        }
+
+        this.ref.close(this.transactionForm.value);
+    }
 
 }
