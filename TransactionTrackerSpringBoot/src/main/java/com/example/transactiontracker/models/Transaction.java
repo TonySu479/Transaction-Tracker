599637721f35d9313dfa57121b79187908ef8b1c
Tony <tonysu479@gmail.com>
2022-03-29 19:50:29 +0200

    Update Transaction.java

--- TransactionTrackerSpringBoot/src/main/java/com/example/transactiontracker/models/Transaction.java
+++ TransactionTrackerSpringBoot/src/main/java/com/example/transactiontracker/models/Transaction.java
@@ -6,18 +6,15 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.util.Calendar;
-import java.util.List;
 
 @Data
 @Entity
 @Table(name = "transactions")
 @NoArgsConstructor
+@AllArgsConstructor
 public class Transaction extends BaseEntity{
 
     @Column(name = "created_at")
     private Calendar createdAt;
 
-    public Transaction(Calendar createdAt){
-        this.createdAt = createdAt;
-    }
 }
