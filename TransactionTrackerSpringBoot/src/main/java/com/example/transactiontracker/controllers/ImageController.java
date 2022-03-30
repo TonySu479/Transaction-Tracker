6fdb7f144afeae93837a197f87bb6d8d7b8ca595
Tony <tonysu479@gmail.com>
2022-03-30 23:23:55 +0200

    Images controller fix and permit all on static images

--- TransactionTrackerSpringBoot/src/main/java/com/example/transactiontracker/controllers/ImageController.java
+++ TransactionTrackerSpringBoot/src/main/java/com/example/transactiontracker/controllers/ImageController.java
@@ -8,13 +8,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,8 +20,6 @@ public class ImageController {
 
     @GetMapping("/images/{imageName}")
     public ResponseEntity<Resource> getImage(@PathVariable("imageName") String imageName) throws IOException {
-        System.out.println("hello");
-        System.out.println(System.getProperty("user.dir"));
         ByteArrayResource inputStream = new ByteArrayResource(
                 Files.readAllBytes(Paths.get(
                         "./TransactionTrackerSpringBoot/assets/images/" + imageName
--- TransactionTrackerSpringBoot/src/main/java/com/example/transactiontracker/security/WebSecurityConfig.java
+++ TransactionTrackerSpringBoot/src/main/java/com/example/transactiontracker/security/WebSecurityConfig.java
@@ -57,8 +57,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/api/auth/**").permitAll()
                 .antMatchers("/api/test/**").permitAll()
-//                .anyRequest().authenticated();
-        .anyRequest().permitAll();
+                .antMatchers("/images/**").permitAll()
+                .anyRequest().authenticated();
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 
