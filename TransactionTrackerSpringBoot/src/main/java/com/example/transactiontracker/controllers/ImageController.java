0382da8cc0067a986a8a0916892adb543c68aafb
Tony <tonysu479@gmail.com>
2022-04-01 15:15:46 +0200

    Image controller getMapping fix + cleanup

--- TransactionTrackerSpringBoot/src/main/java/com/example/transactiontracker/controllers/ImageController.java
+++ TransactionTrackerSpringBoot/src/main/java/com/example/transactiontracker/controllers/ImageController.java
@@ -2,31 +2,24 @@ package com.example.transactiontracker.controllers;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.nio.file.FileSystem;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
 @RestController
 @RequiredArgsConstructor
 public class ImageController {
-
-    @GetMapping("/images/{imageName}")
+    @GetMapping(value = "/images/{imageName}", produces = MediaType.IMAGE_JPEG_VALUE)
     public ResponseEntity<Resource> getImage(@PathVariable("imageName") String imageName) throws IOException {
-        ByteArrayResource inputStream = new ByteArrayResource(
-                Files.readAllBytes(Paths.get(
-                        "./TransactionTrackerSpringBoot/assets/images/" + imageName
-                )));
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .contentLength(inputStream.contentLength())
-                .body(inputStream);
+        FileSystemResource inputStream = new FileSystemResource("./TransactionTrackerSpringBoot/assets/images/" + imageName);
+        return new ResponseEntity<>(inputStream, HttpStatus.OK);
     }
-
 }
