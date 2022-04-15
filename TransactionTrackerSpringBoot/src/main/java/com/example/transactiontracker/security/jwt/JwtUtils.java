010168e1a64f50013017c5d92024a4fa5a4af309
Tony <tonysu479@gmail.com>
2022-04-15 14:09:15 +0200

    Documentation: Spring Security

--- TransactionTrackerSpringBoot/src/main/java/com/example/transactiontracker/security/jwt/JwtUtils.java
+++ TransactionTrackerSpringBoot/src/main/java/com/example/transactiontracker/security/jwt/JwtUtils.java
@@ -30,20 +30,20 @@ public class JwtUtils {
     }
 
     public boolean validateJwtToken(String authToken) {
+
         try {
             Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(authToken);
             return true;
-        } catch (SignatureException e) {
-            logger.error("Invalid JWT signature: {}", e.getMessage());
-        } catch (MalformedJwtException e) {
-            logger.error("Invalid JWT token: {}", e.getMessage());
         } catch (ExpiredJwtException e) {
             logger.error("JWT token is expired: {}", e.getMessage());
         } catch (UnsupportedJwtException e) {
             logger.error("JWT token is unsupported: {}", e.getMessage());
         } catch (IllegalArgumentException e) {
             logger.error("JWT claims string is empty: {}", e.getMessage());
+        } catch (Exception e) {
+            logger.error("Invalid JWT signature: {}", e.getMessage());
         }
+
         return false;
     }
 }
