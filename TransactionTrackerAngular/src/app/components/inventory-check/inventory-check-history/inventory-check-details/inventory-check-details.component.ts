cc6e82de28169875172919a5431b9a83049f96ce
Tony <tonysu479@gmail.com>
2022-05-02 11:42:57 +0200

    Export Inventory-Check Details as pdf function

--- TransactionTrackerAngular/src/app/components/inventory-check/inventory-check-history/inventory-check-details/inventory-check-details.component.ts
+++ TransactionTrackerAngular/src/app/components/inventory-check/inventory-check-history/inventory-check-details/inventory-check-details.component.ts
@@ -5,6 +5,8 @@ import {DialogService} from "primeng/dynamicdialog";
 import {InventoryCheck} from "../../../../model/inventory-check";
 import {ActivatedRoute} from "@angular/router";
 import {InventoryCheckDetailsService} from "../../../../service/inventory-check-details.service";
+import jsPDF from 'jspdf'
+import 'jspdf-autotable'
 
 @Component({
     selector: 'app-inventory-check-details',
@@ -18,6 +20,10 @@ export class InventoryCheckDetailsComponent implements OnInit {
 
     inventoryCheck: InventoryCheck;
 
+    cols: any[];
+
+    exportColumns: any[];
+
     constructor(private messageService: MessageService,
                 private confirmationService: ConfirmationService,
                 private route: ActivatedRoute,
@@ -29,6 +35,33 @@ export class InventoryCheckDetailsComponent implements OnInit {
         this.inventoryCheckDetailsService.getInventoryCheckDetailsByInventoryCheckId(this.inventoryCheck.id).subscribe(
             data => this.inventoryCheckDetails = data
         );
+
+        this.cols = [
+            {field: 'code', header: 'Code'},
+            {field: 'name', header: 'Name'},
+            {field: 'category', header: 'Category'},
+            {field: 'trackedQuantity', header: 'TrackedQuantity'},
+            {field: 'inputtedQuantity', header: 'InputtedQuantity'},
+            {field: 'difference', header: 'Difference'}
+        ];
+
+        this.exportColumns = this.cols.map(col => ({title: col.header, dataKey: col.field}));
+    }
+
+    exportPdf() {
+
+        const doc = new jsPDF() as any;
+        doc.autoTable(this.exportColumns, this.inventoryCheckDetails.map(detail => {
+            return {
+                ...detail,
+                code: detail.product.code,
+                name: detail.product.name,
+                category: detail.product.category['name'],
+                difference: detail.trackedQuantity - detail.inputtedQuantity
+            }
+        }));
+        doc.save('inventory_check.pdf');
+
     }
 
 }
